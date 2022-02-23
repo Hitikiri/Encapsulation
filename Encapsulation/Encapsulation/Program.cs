69d379502ac38e66c78e93c0a878e3aaa3cf4ad3
bekiricli <82258691+Hitikiri@users.noreply.github.com>
2022-02-23 18:55:29 +0300

    Encaps

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectange rectange = new Rectange();
+            rectange.a = 3;
+            rectange.b = 5;
+            Console.WriteLine(rectange.CalculateArea());
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation/Rectange.cs
@@ -0,0 +1,47 @@
+﻿namespace Encapsulation
+{
+    public class Rectange
+    {
+        
+        private int _a;
+        
+        private int _b;
+
+        public int a
+        {
+            get { return _a;}
+            set
+            {
+                if (value<0)
+                {
+                    _a = 0;
+                }
+                else
+                {
+                    _a = value;
+                }
+            }
+        }
+
+        public int b
+        {
+            get { return _b;}
+            set
+            {
+                if (value<0)
+                {
+                    _b = 0;
+                }
+                else
+                {
+                    _b = value;
+                }
+                
+            }
+        }
+        public int CalculateArea()
+        {
+            return a * b;
+        }
+    }
+}
